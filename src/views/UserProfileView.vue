41d283ec8109c492a09b309916fe85f35a788df1
RoyChan1995 <roychan19950119@gmail.com>
2023-10-02 18:14:49 +0800

    Update UserProfileView.vue

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -32,10 +32,10 @@
               <div class="card-body">
                 <div class="row">
                   <div class="col-sm-3">
-                    <p class="mb-0">Full Name</p>
+                    <p class="mb-0">Username</p>
                   </div>
                   <div class="col-sm-9">
-                    <p class="text-muted mb-0">Johnatan Smith</p>
+                    <p class="text-muted mb-0">{{  }}</p>
                   </div>
                 </div>
                 <hr>
@@ -101,6 +101,9 @@ export default {
   // this is data, website will reload if this change
   data() {
     return {
+      fullname: this.fullName,
+      username: "",
+      eamil: "",
     }
   },
 
@@ -119,6 +122,7 @@ export default {
     //dont forget to use this keyword
                    // this is a reference to the backend URL in .env.local file
     this.axios.get(`${import.meta.env.VITE_BACKEND}/user/joshua`).then( response => {
+      console.log(response)
       }
     ).catch ( error => {
     })
