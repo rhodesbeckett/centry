f22db002291884fdaa71ebc4cdc337e498c7e386
RoyChan1995 <roychan19950119@gmail.com>
2023-10-24 00:00:03 +0800

    add popover

--- src/components/GreenBtn.vue
+++ src/components/GreenBtn.vue
@@ -1,9 +1,26 @@
 
 
 <template>
-<div class="d-flex justify-content-center py-3">
-    <button  class="btn btn-success  btn-lg gradient-custom-4 text-white subtitle" >
+<div ref="btn" data-bs-placement="top" class="d-flex justify-content-center py-3" data-bs-toggle="popover" data-bs-trigger="hover focus" 
+    :data-bs-content="disabled && disabledPopover" tabindex="0">
+    <button :disabled="disabled"  class="btn btn-success  btn-lg gradient-custom-4 text-white subtitle" >
         <slot></slot>
     </button>
 </div>
 </template>
+
+<script>
+import * as bootstrap from 'bootstrap';
+
+export default {
+    props: ['disabled', 'disabledPopover'],
+    data(){
+        return {
+            btn : null
+        }
+    },
+    mounted(){
+        this.btn = new bootstrap.Popover(this.$refs.btn)
+    }
+}
+</script>
