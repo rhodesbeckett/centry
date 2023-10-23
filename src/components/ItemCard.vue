d6935635bc5a2c9a0664cd4617cce97ec8094806
RoyChan1995 <roychan19950119@gmail.com>
2023-10-23 14:54:15 +0800

    only 2 tags

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -5,7 +5,7 @@
             <p class="card-text subtitle mx-3 mb-3">
               <span class="subtitleBold">Item category:</span> {{ item.category }}<br>
               <span class="subtitleBold">Item condition:</span> {{ item.condition }} <br>
-              <span class="subtitleBold">Item tags:</span> {{ item.tags.join(", ") }}<br>
+              <span class="subtitleBold">Item tags:</span> {{ item.tags.slice(0,2).join(", ") }}<br>
               <span class="subtitleBold">Views </span> {{ item.views }}<br>
               <span class="subtitleBold">Owner </span> {{ item.user.username }}<br>
             </p>
