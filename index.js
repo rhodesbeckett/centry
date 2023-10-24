d9ebf7073f76a3113673a4f33ee727b91914c1c2
RoyChan1995 <roychan19950119@gmail.com>
2023-10-24 19:27:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import ViteExpress from "vite-express";
 const app = express();
 
 app.get("/message", (_, res) => res.send("Hello from express!"));
-// ViteExpress.config({ mode: "production" })
+ViteExpress.config({ mode: "production" })
 
 ViteExpress.listen(app, process.env.PORT || 5173, () => console.log("Server is listening..."));
