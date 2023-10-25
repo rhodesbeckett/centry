883c651d124986a86708bb094ea44df7c5989422
RoyChan1995 <roychan19950119@gmail.com>
2023-10-25 19:36:03 +0800

    leaflet marker fix

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,3 +1,11 @@
 import placeholder from '../assets/images/scott-lord-PiqZfESKt3k-unsplash.jpg'
 import pinPicture from './images/EMOJICON.png'
 export {placeholder,pinPicture}
+
+import L from 'leaflet'
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+ });
