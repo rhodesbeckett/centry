2d96bdc89d8078fc4afb7c00cd86a438d8f54a91
RoyChan1995 <roychan19950119@gmail.com>
2023-10-25 19:40:44 +0800

    fix leaflet

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,10 +2,14 @@ import placeholder from '../assets/images/scott-lord-PiqZfESKt3k-unsplash.jpg'
 import pinPicture from './images/EMOJICON.png'
 export {placeholder,pinPicture}
 
+import iconRetinaUrl from  'leaflet/dist/images/marker-icon-2x.png'
+import iconUrl from 'leaflet/dist/images/marker-icon.png'
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
+
 import L from 'leaflet'
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    iconRetinaUrl: iconRetinaUrl,
+    iconUrl: iconUrl,
+    shadowUrl: shadowUrl,
  });
