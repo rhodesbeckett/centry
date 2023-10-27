8bb6bdd4ca763a381b93d40f07e6f6ee3a739d17
RoyChan1995 <roychan19950119@gmail.com>
2023-10-28 03:57:53 +0800

    removed unnecessary space

--- src/components/MiddleCardForListing.vue
+++ src/components/MiddleCardForListing.vue
@@ -5,7 +5,7 @@
             <div class="col-11 align-self-center m-auto"> 
                 <!-- card -->
                 <div class="card w-100 max-w-100 m-3 pb-0 h-auto m-auto my-5">
-                <div class="card-body  ">
+                <div class="card-body">
                     <slot></slot>
                 </div>
                 </div>
