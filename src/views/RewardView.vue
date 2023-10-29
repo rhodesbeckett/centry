692fa25bece8a0c8ae489f3426b1504b2e574087
RoyChan1995 <roychan19950119@gmail.com>
2023-10-29 21:46:54 +0800

    Update RewardView.vue

--- src/views/RewardView.vue
+++ src/views/RewardView.vue
@@ -16,135 +16,123 @@ import moment from "moment";
     <h1 class="titleBold text-center" style="font-size: xxx-large;">Points summary</h1>
     <div class="text-center">
       <h4><b>Total Points: </b>{{ netPoints }} </h4>
-      <h4><b>Current Tier: </b>{{ tier }} </h4>
+      <h4><b>Current Tier: </b><span :style="{color: tier} ">{{ tier }}</span> </h4>
     </div>
 
-   <MiddleCardForListing>
-    
-    <div class="container-fluid">
-      <div class="row">
-        <!-- <div style="padding-left: 27px;" class="col">
-          <h1 >Points summary: </h1>
-        </div> -->
-        
-        </div>
-        <div class="row">
-        <div class='col-lg-6 justify-content-center overflow-auto' style="height: 60vh;">
-            <div class="white p-3">
-              
-              <h4>Points Information</h4>
-                   
-                <table class="table" style=" text-align: center; border: 1px solid rgb(192, 192, 192);">
-                  <tr style="border: 1px solid rgb(192, 192, 192);">
-                    <th style="padding: 10px;">Action</th>
-                    <th>Points Range</th>
-                  </tr>
-                  <tr style="border: 1px solid rgb(192, 192, 192);">
-                    <td style="padding: 10px;">List Item</td>
-                    <td>+2 Points</td>
-                  </tr>
-                  <tr style="border: 1px solid rgb(192, 192, 192);">
-                    <td style="padding: 10px;">Remove Item</td>
-                    <td>-2 Points</td>
-                  </tr>
-                  <tr style="border: 1px solid rgb(192, 192, 192);">
-                    <td style="padding: 10px;">Trade</td>
-                    <td>+5 Points</td>
-                  </tr>
-                  
-              </table>
-                <br><h4>Tier Information</h4>
-                <table class="table" style=" text-align: center; border: 1px solid rgb(192, 192, 192);">
-              <tr style="border: 1px solid rgb(192, 192, 192);">
-                <th style="padding: 10px;">Tier</th>
-                <th>Points range</th>
-              </tr>
-              <tr style="border: 1px solid rgb(192, 192, 192);">
-                <td style="padding: 10px;">Green</td>
-                <td>0-99</td>
-              </tr>
-              <tr style="border: 1px solid rgb(192, 192, 192);">
-                <td style="padding: 10px;">Silver</td>
-                <td>100-299</td>
-              </tr>
-              <tr style="border: 1px solid rgb(192, 192, 192);">
-                <td style="padding: 10px;">Gold</td>
-                <td>300-499</td>
-              </tr>
-              <tr style="border: 1px solid rgb(192, 192, 192);">
-                <td style="padding: 10px;">Superstar</td>
-                <td>500+</td>
-              </tr>
-            </table>
-            <p>Tier is based on accumulated points - before rewards redemption of which you have {{ accPoints }} points</p>
-                <br>
-                <!-- start -->
-                <div class="header">
-                  <h4>Transactions</h4>
+    <div class="container-fluid mt-5" style="max-width: 80%;">
+      <div class="accordion" id="accordionPanelsStayOpenExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+              Points and tier information
+            </button>
+          </h2>
+          <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+            <div class="accordion-body">
+              <div class="container-fluid">
+                <div class="row align-items-start">
+                  <div class='col-md-6 col-12 justify-content-center'>
+                    <h4>Point System</h4>
+                    <table class="table" style=" text-align: center; border: 1px solid rgb(192, 192, 192);">
+                      <tr style="border: 1px solid rgb(192, 192, 192);">
+                        <th style="padding: 10px;">Action</th>
+                        <th>Points Range</th>
+                      </tr>
+                      <tr style="border: 1px solid rgb(192, 192, 192);">
+                        <td style="padding: 10px;">List Item</td>
+                        <td>+2 Points</td>
+                      </tr>
+                      <tr style="border: 1px solid rgb(192, 192, 192);">
+                        <td style="padding: 10px;">Remove Item</td>
+                        <td>-2 Points</td>
+                      </tr>
+                      <tr style="border: 1px solid rgb(192, 192, 192);">
+                        <td style="padding: 10px;">Trade</td>
+                        <td>+5 Points</td>
+                      </tr>
+                    </table>
+                  </div>
+                <div class='col-md-6 col-12 justify-content-center'>
+                  <h4>Tier System</h4>
+                  <table class="table" style=" text-align: center; border: 1px solid rgb(192, 192, 192);">
+                    <tr style="border: 1px solid rgb(192, 192, 192);">
+                      <th style="padding: 10px;">Tier</th>
+                      <th>Points range</th>
+                    </tr>
+                    <tr style="border: 1px solid rgb(192, 192, 192);">
+                      <td style="padding: 10px;">Green</td>
+                      <td>0-99</td>
+                    </tr>
+                    <tr style="border: 1px solid rgb(192, 192, 192);">
+                      <td style="padding: 10px;">Silver</td>
+                      <td>100-299</td>
+                    </tr>
+                    <tr style="border: 1px solid rgb(192, 192, 192);">
+                      <td style="padding: 10px;">Gold</td>
+                      <td>300-499</td>
+                    </tr>
+                    <tr style="border: 1px solid rgb(192, 192, 192);">
+                      <td style="padding: 10px;">Superstar</td>
+                      <td>500+</td>
+                    </tr>
+                  </table>
+                  <div>Your tier is determined by your total points before rewards redemption!</div>
                 </div>
-               
-
-                <div class="card" style="width: 100%; height: auto;">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item" :class="{'bg-success text-white' : rewards_rewardName.includes(transaction.rewardName) }" v-for="transaction in transactions">
-                            <!-- <b>{{ transaction.rewardName}}</b>  -->
-                            <div v-if="transaction.rewardName=='addListedItem'"><b>List Item</b></div>
-                            <div v-else-if="transaction.rewardName=='trade'"><b>Successful Trade</b></div>
-                            <div v-else><b>Redeemed: {{ transaction.rewardName}}</b></div>
-                            Date: {{ moment(transaction.createdAt).format("DD/MM/YYYY")}}
-
-                            <br>Points: {{ (transaction.points > 0 ? "+" : "")+  transaction.points }} points
-                          
-                        </li>
-                        <li class="list-group-item" v-if="transactions.length == 0">
-                          Empty
-                          
-                        </li>
-                    </ul>
-
-                    
-                    </div>
-                <!-- end -->
-                
-
+              </div>
             </div>
-          
-      </div>
-
-        <div class='col  justify-content-center overflow-auto' style="height: 60vh;">
-          <div class="white p-3">
-
-            <!-- <h4>Accumulated Points: {{ accPoints }}</h4> -->
-            <!-- <h4>Total Points: {{ netPoints }}</h4>
-            <h4>Current Tier: {{ tier }}</h4> -->
-               <!-- start -->
-
-                <div class="header">
-                  <h4>Rewards</h4>
-                </div>
-                
-                
-                <div class="card" style="width: 100%; height: auto;">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item " v-for="item in rewards">
-                        <b>{{ item.prizeTitle }} ({{ Math.abs( item.points) }} points)</b>
-                        <br>{{ item.prizeDescription }}
-                        <p> Only for first {{ item.max }} redeemers. While stocks last!</p>
-                        <GreenBtn @click="(Math.abs(item.points) <= netPoints) && redeem(item.rewardName)" v-if="Math.abs(item.points) <= netPoints">Redeem</GreenBtn>
-                         <button v-else disabled class="btn btn-lg btn-warning">Not enough points!</button>
-                    </li>
-                </ul>
-                </div>
-                <!-- end -->
-    
-
+          </div>
         </div>
-        
       </div>
     </div>
-</div>
+  </div>
 
-   </MiddleCardForListing>
+  <div class="container-fluid p-0">
+    <div class="row align-items-start">
+      <div class="col-md-6">
+        <MiddleCardForListing>
+          <div class="header">
+            <h3>Point transactions</h3>
+          </div>
+          <div class="card" style="width: 100%; height: auto;">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item" :class="{'bg-success text-white' : rewards_rewardName.includes(transaction.rewardName) }" v-for="transaction in transactions">
+                  <!-- <b>{{ transaction.rewardName}}</b>  -->
+                  <div v-if="transaction.rewardName=='addListedItem'"><b>List Item</b></div>
+                  <div v-else-if="transaction.rewardName=='trade'"><b>Successful Trade</b></div>
+                  <div v-else><b>Redeemed: {{ transaction.rewardName}}</b></div>
+                  Date: {{ moment(transaction.createdAt).format("DD/MM/YYYY")}}
+                  <br>Points: {{ (transaction.points > 0 ? "+" : "")+  transaction.points }} points
+        
+              </li>
+              <li class="list-group-item" v-if="transactions.length == 0">
+                No point transactions yet!
+              </li>
+            </ul>
+          </div>
+        </MiddleCardForListing>
+      </div>
+      <div class="col-md-6">
+        <MiddleCardForListing>
+          <div class="header">
+            <h3>Rewards</h3>
+          </div>
+          <div class="card" style="width: 100%; height: auto;">
+          <ul class="list-group list-group-flush">
+              <li class="list-group-item " v-for="item in rewards">
+                  <b>{{ item.prizeTitle }} ({{ Math.abs( item.points) }} points)</b>
+                  <br>{{ item.prizeDescription }}
+                  Only for first {{ item.max }} redeemers. While stocks last!
+                  <div class="my-2">
+                    <GreenBtn @click="(Math.abs(item.points) <= netPoints) && redeem(item.rewardName)" v-if="Math.abs(item.points) <= netPoints">Redeem</GreenBtn>
+                    <button v-else disabled class="btn btn-md btn-dark">Not enough points!</button>
+                  </div>
+              </li>
+          </ul>
+          </div>
+        </MiddleCardForListing>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped> 
