5278ae861ebe182f113e6ba5cd1b2ea2a83f3827
RoyChan1995 <roychan19950119@gmail.com>
2023-10-31 02:32:44 +0800

    accommodate  long names

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -3,19 +3,57 @@
     <img v-lazy="item.photoURLs.length > 0 ? item.photoURLs[0] : placeholder" class="card-img-top w-100 rounded">
     <div class="container p-0">
       <div class="row">
-        <h4 class="col card-title subtitle text-truncate mx-3 mt-3">{{ item.itemName }}</h4>
+        <h4 class="col card-title subtitle mx-3 mt-3 twoLines" id="itemname">{{ item.itemName }}</h4>
         <span class="col-4 badge text-bg-danger rounded-pill me-4" v-if="item.done">Traded</span>
       </div>
     </div>
+    <div class="card-text subtitle text-truncate  mx-3 text-capitalize mb-2">
+        {{ item.condition }} 
+        
+        <br class="d-inline d-sm-none"> 
+        
+        {{ item.category }}<br>
+    </div>
+
+    <div class="d-none d-sm-inline mx-3 twoLines">
+      <span class="subtitleBold">Tags:</span> <span></span> {{ item.tags.length > 0 ? item.tags.join(", ") : "-" }}
+    </div>
     <div class="card-text subtitle text-truncate mx-3 mb-3">
-      <span class="subtitleBold">Category:</span> {{ item.category }}<br>
-      <span class="subtitleBold">Condition:</span> {{ item.condition }} <br>
-      <span class="subtitleBold">Tags:</span> {{ item.tags.length > 0 ? item.tags.join(", ") : "-" }}<br>
       <span class="subtitleBold">Views: </span> {{ item.views }}<br>
-      <span class="subtitleBold">Listed by: </span> {{ item.user.username }}<br>
+      <span class="subtitleBold">By: </span> {{ item.user.username }}<br>
     </div>
   </div>
 </template>
+
+<style scoped>
+.twoLines {
+  
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;  
+  overflow: hidden;
+
+  height: 2.3em;
+  line-height: 1.1;
+  }
+
+
+@media (max-width: 576px) {
+  .twoLines {
+
+
+    display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;  
+  overflow: hidden;
+
+  height: 4.5em;
+  line-height:1.1;
+
+
+  }
+}
+</style>
 
 <script>
 import {placeholder} from '../assets/assets'
