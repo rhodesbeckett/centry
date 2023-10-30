7d0eb2b23b01b0f5f600ae907e47d48572265008
RoyChan1995 <roychan19950119@gmail.com>
2023-10-30 10:54:58 +0800

    update user market

--- src/views/item/UserMarketplaceView.vue
+++ src/views/item/UserMarketplaceView.vue
@@ -39,6 +39,39 @@ import MiddleCardForListing from '../../components/MiddleCardForListing.vue';
       </button>
     </div>
 
+    <!-- modal start -->
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      Instructions
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Welcome to the marketplace!</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="trending">
+              See what other users are interested in! browse through the trending items!
+            </div>
+            <br>
+            <div class="search">
+              Have an item in mind? try searching by item name, or use the filters to narrow your search!
+            </div>
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- modal end -->
+
 
     <!-- trending horizontal scroll -->
 
